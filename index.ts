3245f81957c3aaf306eb99a3f5b68cb637af2f75
sumitmckv <sumitmckv14@gmail.com>
2020-09-01 18:22:02 +0530

    adding vercel config

--- index.ts
+++ index.ts
@@ -6,12 +6,15 @@ import categoriesRouter from "./api/categories";
 import tagsRouter from "./api/tags";
 
 const app = express();
-app.use(cors())
+app.use(cors({
+    origin: process.env.CORS_ORIGIN
+}));
 app.get('/', (req, res) => res.send("Hello from Truecaller Blog API"));
 app.use('/api/posts', postRouter);
 app.use('/api/categories', categoriesRouter);
 app.use('/api/tags', tagsRouter);
 
-app.listen(PORT, () => {
-    console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
+const port = process.env.PORT || PORT;
+app.listen(port, () => {
+    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
